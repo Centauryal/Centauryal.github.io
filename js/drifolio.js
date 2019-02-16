8c7e0e1913ee1427c92bb106ff903d0aee49f872
alfacentaury <alfa.arnialfa@gmail.com>
2019-02-16 21:26:58 +0700

    change to https

--- js/drifolio.js
+++ js/drifolio.js
@@ -30,16 +30,16 @@ $(function() {
 	// SOME VARIABLES
 	var button = '.dribbble-follow-button',
 		label = $(button).text(),
-		username = $('a'+button).attr('href').toLowerCase().replace('http://dribbble.com/', ''),
+		username = $('a'+button).attr('href').toLowerCase().replace('https://dribbble.com/', ''),
 		disableCount = $(button).attr('class');
 
 	// DISPLAYED WHEN THE API IS NOT RESPONDING
 	$(button).wrap('<div class="dribbble-follow-button" />').removeClass().addClass('label').html('<i></i> '+label);
 
 	// REQUESTS USER'S DATA FROM DRIBBBLE'S API AND APPENDS IT
-	$.getJSON('http://api.dribbble.com/players/'+username+'?callback=?', function(data) {
+	$.getJSON('https://api.dribbble.com/players/'+username+'?callback=?', function(data) {
 		$(button).wrap('<div class="dribbble-follow-button '+disableCount+'" />')
-        .parent().html('<a class="label" href="http://dribbble.com/'+username+'" target="_blank"><i></i>'+label+'</a><a class="count" href="http://dribbble.com/'+username+'/followers" target="_blank"><i></i><u></u>'+data.followers_count+' followers</a>');
+        .parent().html('<a class="label" href="https://dribbble.com/'+username+'" target="_blank"><i></i>'+label+'</a><a class="count" href="http://dribbble.com/'+username+'/followers" target="_blank"><i></i><u></u>'+data.followers_count+' followers</a>');
 		$(button+'.disableCount').find('.count').remove();
 	});
 
