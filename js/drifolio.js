ec3ee50d87d28218b1cf3f382d0149d204d22ba9
alfacentaury <alfa.arnialfa@gmail.com>
2020-03-17 13:24:00 +0700

    Edit image

--- js/drifolio.js
+++ js/drifolio.js
@@ -1,7 +1,7 @@
 //==========================================================
 //Replace srizon with your dribbble username
 //==========================================================
-$.jribbble.getShotsByPlayerId('', function (playerShots) {
+/* $.jribbble.getShotsByPlayerId('', function (playerShots) {
     var html = [];
 
 
@@ -43,84 +43,94 @@ $(function() {
 		$(button+'.disableCount').find('.count').remove();
 	});
 
-});
-
+}); */
 
 //========================
 //PRELOADER
 //========================
-$(window).load(function() { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow');
-    // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow':'visible'});
-})
+$(window).load(function() {
+  // makes sure the whole site is loaded
+  $("#status").fadeOut(); // will first fade out the loading animation
+  $("#preloader")
+    .delay(350)
+    .fadeOut("slow");
+  // will fade out the white DIV that covers the website.
+  $("body")
+    .delay(350)
+    .css({ overflow: "visible" });
+});
 //========================
 //CUSTOM SCROLLBAR
 //========================
 $("html").niceScroll({
-    mousescrollstep: 70,
-    cursorcolor: "#ea9312",
-    cursorwidth: "5px",
-    cursorborderradius: "10px",
-    cursorborder: "none",
+  mousescrollstep: 70,
+  cursorcolor: "#ea9312",
+  cursorwidth: "5px",
+  cursorborderradius: "10px",
+  cursorborder: "none"
 });
-
 
 //========================
 //SMOOTHSCROLL
 //========================
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  $("a[href*=#]:not([href=#])").click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
         return false;
       }
     }
   });
 });
 
-
 //========================
 //NAVBAR
 //========================
-(function ($) {
-  $(document).ready(function(){
-
+(function($) {
+  $(document).ready(function() {
     // hide .navbar first
     $(".navbar").hide();
 
     // fade in .navbar
-    $(function () {
-        $(window).scroll(function () {
-
-                 // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 40) {
-                $('.navbar')
-                .removeClass('animated fadeOutUp')
-                .addClass('animated fadeInDown')
-                .fadeIn();
-                
-            } else {
-                $('.navbar')
-                .removeClass('animated fadeInDown')
-                .addClass('animated fadeOutUp')
-                .fadeOut();
-            }
-        });
+    $(function() {
+      $(window).scroll(function() {
+        // set distance user needs to scroll before we start fadeIn
+        if ($(this).scrollTop() > 40) {
+          $(".navbar")
+            .removeClass("animated fadeOutUp")
+            .addClass("animated fadeInDown")
+            .fadeIn();
+        } else {
+          $(".navbar")
+            .removeClass("animated fadeInDown")
+            .addClass("animated fadeOutUp")
+            .fadeOut();
+        }
+      });
     });
-
-});
-  }(jQuery));
+  });
+})(jQuery);
 
 //========================
 //icon hover effect
 //========================
-$('#services img').hover(
-       function(){ $(this).addClass('animated pulse') },
-       function(){ $(this).removeClass('animated pulse') })
+$("#services img").hover(
+  function() {
+    $(this).addClass("animated pulse");
+  },
+  function() {
+    $(this).removeClass("animated pulse");
+  }
+);
